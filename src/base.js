0d8b0ff6db3cded53c8bd9a7f55ed55f099b73ae
ibrahima DANSOKO <ibrahima.dansoko@ftprod.fr>
2019-05-17 17:10:56 +0200

    add .env

--- src/base.js
+++ src/base.js
@@ -1,15 +1,17 @@
 import Rebase from "re-base";
 import firebase from "firebase/app";
 import "firebase/database";
+import dotenv from "dotenv";
+dotenv.config();
 
 const firebaseApp = firebase.initializeApp({
-  apiKey: "AIzaSyDGfpKk-G5ID9aJ0OVQRqPp0ZFINHEEXqc",
-  authDomain: "recetteapp-53f6b.firebaseapp.com",
-  databaseURL: "https://recetteapp-53f6b.firebaseio.com",
-  projectId: "recetteapp-53f6b",
-  storageBucket: "recetteapp-53f6b.appspot.com",
-  messagingSenderId: "535216314057",
-  appId: "1:535216314057:web:002b53650217f4ba"
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  databaseURL: process.env.databaseURL,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId
 });
 
 const base = Rebase.createClass(firebaseApp.database());
