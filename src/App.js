5fc7c0b327bde1ece433e6dd1656990229ee3c92
ibrahima DANSOKO <ibrahima.dansoko@ftprod.fr>
2019-05-17 16:59:19 +0200

    add form and connection firebase

--- src/App.js
+++ src/App.js
@@ -2,27 +2,39 @@ import React, { Component } from "react";
 import "./App.css";
 
 import Header from "./components/Header";
-import recettes from './recettes';
-import Admin from './components/admin';
-import Card from './components/Card'
-
+import recettes from "./recettes";
+import Admin from "./components/admin";
+import Card from "./components/Card";
+import base from "./base";
 class App extends Component {
   state = {
     pseudo: this.props.match.params.pseudo,
-    recettes : {}
+    recettes: {}
   };
-chargerExemple = () => this.setState({recettes})
+  componentDidMount() {
+    this.ref = base.syncState(`/${this.state.pseudo}/recettes`, {
+      context: this,
+      state: "recettes"
+    });
+  }
+  componentWillUnmount() {
+    base.removeBinding(this.ref);
+  }
+  ajouterRecette = recette => {
+    const recettes = { ...this.state.recettes };
+    recettes[`recete-${Date.now()}`] = recette;
+    this.setState({ recettes });
+  };
+  chargerExemple = () => this.setState({ recettes });
   render() {
-    const cards = Object.keys(this.state.recettes).map(item =>
-      <Card details= {this.state.recettes[item]} key= {item} />
-    )
-    return ( 
+    const cards = Object.keys(this.state.recettes).map(item => (
+      <Card details={this.state.recettes[item]} key={item} />
+    ));
+    return (
       <div className="box">
-        <Header pseudo = {this.state.pseudo}/>
-        <div className="cards">
-            {cards}
-        </div>
-      <Admin chargerExemple ={this.chargerExemple}></Admin>
+        <Header pseudo={this.state.pseudo} />
+        <div className="cards">{cards}</div>
+        <Admin chargerExemple={this.chargerExemple} ajouterRecette = {this.state.ajouterRecette} />
       </div>
     );
   }
--- src/base.js
+++ src/base.js
@@ -1,17 +1,21 @@
-import Rebase from 're-base'
-import firebase from 'firebase/app'
-import 'firebase/database'
+import Rebase from "re-base";
+import firebase from "firebase/app";
+import "firebase/database";
 
 const firebaseApp = firebase.initializeApp({
-  apiKey: 'xxx',
-  authDomain: 'xxx',
-  databaseURL: 'xxx'
-})
+  apiKey: "AIzaSyDGfpKk-G5ID9aJ0OVQRqPp0ZFINHEEXqc",
+  authDomain: "recetteapp-53f6b.firebaseapp.com",
+  databaseURL: "https://recetteapp-53f6b.firebaseio.com",
+  projectId: "recetteapp-53f6b",
+  storageBucket: "recetteapp-53f6b.appspot.com",
+  messagingSenderId: "535216314057",
+  appId: "1:535216314057:web:002b53650217f4ba"
+});
 
-const base = Rebase.createClass(firebaseApp.database())
+const base = Rebase.createClass(firebaseApp.database());
 
 // This is a named export
-export { firebaseApp }
+export { firebaseApp };
 
 // this is a default export
-export default base
+export default base;
