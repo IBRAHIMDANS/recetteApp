0685884ce4c0e483ee032a1409f773a4ac76db2e
ibrahima DANSOKO <ibrahima.dansoko@ftprod.fr>
2019-05-17 11:46:40 +0200

    init recete app

---
+++ src/components/NotFound.js
@@ -0,0 +1,9 @@
+import React, { useState } from "react";
+import { Button } from "antd";
+import { Link } from "react-router-dom";
+
+const NotFound = () => {
+  return <h2 className="notFound"> Sorry NotFound </h2>;
+};
+
+export default NotFound;
